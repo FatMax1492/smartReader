0ca1a0e2b5781ad16cebf44497fbbc4b70e5fe8c
FatMax1492 <59387233+FatMax1492@users.noreply.github.com>
2024-05-19 03:27:43 +0200

    Expanded with UTF-8 characterset

--- Firefox/content_scripts/isItLetter.js
+++ Firefox/content_scripts/isItLetter.js
@@ -11,8 +11,11 @@ function isItLetter(char) {
     */
 
     var letter = (char > 64 && char < 91) || (char > 96 && char < 123) 
-                || (char > 191 && char < 208) || (char > 208 && char < 215) 
-                || (char > 215 && char < 247) || (char > 247 && char < 256); 
+                || (char > 185 && char < 187) || (char > 189 && char < 215)
+				|| (char > 215 && char < 383) || (char > 461 && char < 469)
+				|| (char > 476 && char < 478) || (char > 485 && char < 488)
+				|| (char > 513 && char < 516) || (char > 521 && char < 524)
+				|| (char > 533 && char < 540) || (char > 533 && char < 540);
 
     return letter;
 }
